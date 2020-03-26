2ea7cd6632fc4098d9e1a30847d2fe0cf5e182b2
Soares Azil <Sdomingos@unilurio.ac.mz>
2020-03-26 11:36:11 -0700

    primeicomete soaka

---
+++ src/main/java/Controladora.java
@@ -0,0 +1,15 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Controladora {
+
+    @GetMapping("/")
+    public ModelAndView index(){
+        ModelAndView model = new ModelAndView("index");
+
+        return model;
+    }
+
+}
